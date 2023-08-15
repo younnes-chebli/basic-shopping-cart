5a5c393d359ace8bd230d7951790a5ee42d86c63
Younnes Chebli <younnesc@gmail.com>
2023-08-15 02:50:41 +0200

    added shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -26,6 +26,6 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}>('/assets/shipping.json');
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
