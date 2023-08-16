cf9bdf6180143b9b6beb261cc1d5bcd5ce335f65
Younnes Chebli <younnesc@gmail.com>
2023-08-16 10:09:01 +0200

    feat(checkout form): defined the checkout form model

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,7 +10,19 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
+
+  OnSubmit(): void {
+    this.items = this.cartService.clearItems();
+    console.warn('>Your Order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
